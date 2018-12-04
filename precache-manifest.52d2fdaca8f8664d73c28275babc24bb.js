7d2dc844ae26163493f22edac18ab8466be64a54
iGitScor <sebastien.correaud@gmail.com>
2018-12-04 14:28:16 +0100

    Automatic deployment update

---
+++ precache-manifest.52d2fdaca8f8664d73c28275babc24bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "177c0dcdb211038507f3",
+    "url": "http://job.iscor.me/static/js/runtime~main.177c0dcd.js"
+  },
+  {
+    "revision": "a93e9e86d6d73973d409",
+    "url": "http://job.iscor.me/static/js/main.a93e9e86.chunk.js"
+  },
+  {
+    "revision": "a73336189569e12f8a07",
+    "url": "http://job.iscor.me/static/js/1.a7333618.chunk.js"
+  },
+  {
+    "revision": "a93e9e86d6d73973d409",
+    "url": "http://job.iscor.me/static/css/main.d1f1b48b.chunk.css"
+  },
+  {
+    "revision": "4f208698363c435e70c381c1a5a4a41e",
+    "url": "http://job.iscor.me/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f7ce9a690c073a3d000d3bb74b78df04.js"
+  "http://job.iscor.me/precache-manifest.52d2fdaca8f8664d73c28275babc24bb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("http://job.iscor.me/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
